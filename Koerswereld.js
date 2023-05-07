2bb07817d7f652a2287bf9c097f9de98538c2a23
MilanBerckmoes <Milan.Berckmoes@UGent.be>
2023-05-07 13:18:56 +0200

    koerswereld in orde gezet om eerste versie vd robot te tonen

--- Koerswereld.js
+++ Koerswereld.js
@@ -1,5 +1,23 @@
+let myRobot;
 let backgroundImg;
 
 function preload() {
+
     backgroundImg = loadImage('data/cobbles.jpg');
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    backgroundImg.resize(width, height);
+    myRobot = new robot();
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    backgroundImg.resize(width, height);
+}
+
+function draw() {
+    image(backgroundImg, 0, 0);
+    myRobot.display();
 }
