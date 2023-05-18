60ad8d2dbff67f21be5ef5fd8cb38a5ec0388db5
MilanBerckmoes <Milan.Berckmoes@UGent.be>
2023-05-18 15:32:44 +0200

    sounds aangepast

--- Koerswereld.js
+++ Koerswereld.js
@@ -31,10 +31,10 @@ function gotWeatherData(data) {
 function preload() {
     backgroundImg = loadImage('data/cobbles.jpg');
     soundFormats('MP3');
-    gameSound = loadSound('data/Rodania.mp3');
-    winningSound = loadSound('data/TomBoonen.mp3');
-    bidonSound = loadSound('data/bidonsound.mp3');
-    spuitSound = loadSound('data/spuitsound.mp3');
+    gameSound = loadSound('data/Rodania');
+    winningSound = loadSound('data/TomBoonen');
+    bidonSound = loadSound('data/bidonsound');
+    spuitSound = loadSound('data/spuitsound');
     //Load current weatherdata in Kortrijk
     loadJSON('https://api.openweathermap.org/data/2.5/weather?q=Kortrijk,be&appid=276a00d1cf36f1c2b52ae48758fce6ea', gotWeatherData);
 }
